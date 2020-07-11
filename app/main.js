e1234b2953905911061cfa6b67fdf9ac9e06f97f
Eric Lau <ericlaucmo@gmail.com>
2020-07-11 13:04:20 -0700

    App start up

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
-console.log('This is an Electron application!');
+// Destructure the functionality we want from the "electron" object
+const { app, BrowserWindow } = require('electron');
+
+// Variable where the program will "live"
+let mainWindow = null;
+
+app.on('ready', () => {
+  // Default the app to not show
+  mainWindow = new BrowserWindow({ show: false });
+
+  // Load the HTML + CSS
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  // Once the HTML + CSS have loaded then show
+  // This is the skip the white flash while the HTML + CSS are loading on start up
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+});
