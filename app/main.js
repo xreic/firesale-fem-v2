faa63432749d3e575992ca956ab44d8884f2965d
Eric Lau <ericlaucmo@gmail.com>
2020-07-11 13:16:53 -0700

    Integrate system native file picker

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
+
 // Destructure the functionality we want from the "electron" object
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 
 // Variable where the program will "live"
 let mainWindow = null;
@@ -11,9 +13,38 @@ app.on('ready', () => {
   // Load the HTML + CSS
   mainWindow.loadFile(`${__dirname}/index.html`);
 
+  // Function call to open the native system file picker
+  getFileFromUser();
+
   // Once the HTML + CSS have loaded then show
   // This is the skip the white flash while the HTML + CSS are loading on start up
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
 });
+
+const getFileFromUser = () => {
+  // Save into the variable the return of whatever is selected in the file picker as an array
+  const files = dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      {
+        name: 'Markdown Files',
+        extensions: ['md', 'mdown', 'markdown', 'marcdown'],
+      },
+      {
+        name: 'Text Files',
+        extensions: ['txt', 'text'],
+      },
+    ],
+  });
+
+  // Early return when no files are selected
+  if (!files) return;
+
+  // Pick and read only the first file from the selection
+  const file = files[0];
+  const content = fs.readFileSync(file).toString();
+
+  console.log(content);
+};
